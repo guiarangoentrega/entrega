a1ee0d488f63a757a1269c318d9f6fe639d1f0e3
guiarangoentrega <48455485+guiarangoentrega@users.noreply.github.com>
2019-03-11 17:30:33 -0300

    Add files via upload

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={		
+	'ApiUrl':"http://www.guiarango.com.br/mobileapp/api",				
+	'DialogDefaultTitle':"Guia Rango",	
+	'APIHasKey':"df284e28ad6eb6e254fa12b787831806",
+	'debug': false
+};
